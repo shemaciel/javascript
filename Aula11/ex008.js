39568a9472bb2ce29eab869e3b459039ddcba0d4
Sheron <103598166+shemaciel@users.noreply.github.com>
2022-10-06 14:34:31 -0300

    atividades
    010

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log(`o console funcionou corretamente!`)
+var vel = 60.5
+console.log (`a velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) {// condição simples
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`drija sempre usando o cinto de segurança!`)
