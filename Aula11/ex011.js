de0ef2a5b5632d263d1627f99fdc4f8b6bf79a87
Sheron <103598166+shemaciel@users.noreply.github.com>
2023-05-16 15:48:19 -0300

    ex 010
    aprendendo

---
+++ Aula11/ex011.js
@@ -0,0 +1,9 @@
+//nesse caso vamos criar uma condição composta: exemplo
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {// se o pais for Brasil a pessoa sera brsileiro
+    console.log('Você é Brasileiro')
+} else { // se não ela sera estrangeira
+    console.log('Você é Estrangeiro')
+
+}
---
+++ Aula12/0x013.js
@@ -0,0 +1,26 @@
+/* console.log(diaSem)
+switch só finciona com numeros inteiros e com strings*/
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+        case 0:
+        console.log('Segunda')
+        break
+        case 0:
+        console.log('Terça')
+        break
+        case 0:
+        console.log('Quarta')
+        break
+        case 0:
+        console.log('Quinta')
+        break
+        case 0:
+        console.log('Sexta')
+        break
+        case 0:
+        console.log('Sabado')
+        break
+}
